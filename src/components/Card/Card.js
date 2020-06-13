9bd4311539dcea13926dbf934e14b9dc087fdb82
nazimkazim <nazik_111@mail.ru>
2020-06-13 11:42:58 +0600

    add cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 function Card(props) {
   return (
-    <div className="card">
+    <div className={`${props.class}-card`}>
       <div className="face face1">
         <div className="content">
-          <img src={props.item.icon} alt=""/>
+          <div className="has-text-centered">
+            <img src={props.item.icon} alt=""/>
+          </div>
           <h3>{props.item.moto}</h3>
         </div>
       </div>
--- src/components/Card/CardRow.js
+++ src/components/Card/CardRow.js
@@ -5,7 +5,7 @@ function CardRow(props) {
   return (
     <div className={`${props.class}-container`}>
       {props.data.map((item, index) => (
-        <Card item={item}/>
+        <Card item={item} class={props.class}/>
       ))}
     </div>
   )
