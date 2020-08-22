519bd3cfb0abff60863bd5452fc5c66b69c7261e
nazimkazim <nazik_111@mail.ru>
2020-08-22 14:40:21 +0600

    add change layout

--- src/pages/SoberEyePage.js
+++ src/pages/SoberEyePage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SoberEyeVirtualGlass from '../images/sober-eye-virtual-glass.webp';
 import Dashboardlaptop from '../images/dashboardlaptop.webp';
+import SoberEyeTarget from '../images/sober-eye-target.PNG';
 
 export default function SoberEyePage() {
 
@@ -11,15 +12,29 @@ export default function SoberEyePage() {
         list: [
           'Портативный и легкий в использовании', 'Не инвазивный', 'Результаты в реальном времени'
         ],
-        image: SoberEyeVirtualGlass,
-        color:'#bfc6df'
+        image: SoberEyeVirtualGlass
       },
       {
         header: 'Корпоративные отчеты в облаке',
         list: [
           'Мониторинг в реальном времени', 'Уведомления и предупреждения', 'Аналитика данных'
         ],
-        image: Dashboardlaptop
+        image: Dashboardlaptop,
+        reversed: true,
+        color: '#bfc6df'
+      },
+      {
+        header: 'SOBEREYE это услуга на основе подписки, включающая',
+        list: [
+          'Тестирующие устройства', 'Облачное програмное обеспечеение', 'Поддержка и обучение'
+        ],
+        image: SoberEyeTarget,
+        extraInfo: {
+          bolded: 'Инновационное решение для обеспечения безопасности на рабочем месте, которое спасает жизни и экономит ваши деньги с точностью 99,9%',
+          subtitle: 'Спроектирован так, чтобы обеспечивать менее одного из тысячи ложных срабатываний.',
+          moto: 'Более безопасное рабочее место в мгновение ока'
+
+        }
       }
 
     ]
@@ -28,22 +43,57 @@ export default function SoberEyePage() {
   return (
     <>
       { data.soberSectionsArr.map((item) => (
-        <div className="sober-eye-section" style={{backgroundColor:item.color}}>
-          <div className="sober-eye-section-half">
-            <div className="sober-eye-section-left-desc-container">
-              <span className="sober-eye-section-left-desc-header">
-                {item.header}
-            </span>
-              <ul className="sober-eye-section-left-desc-list">
-                {item.list.map((i) => (
-                  <li>{i}</li>
-                ))}
-              </ul>
-            </div>
-          </div>
-          <div className="sober-eye-section-half">
-            <img src={ item.image } alt="" />
-          </div>
+        <div className="sober-eye-section" style={ { backgroundColor: item.color } }>
+          { item.reversed ? (
+            <>
+              <div className="sober-eye-section-half">
+                <img src={ item.image } alt="" />
+              </div>
+              <div className="sober-eye-section-half">
+                <div className="sober-eye-section-left-desc-container">
+                  <span className="sober-eye-section-left-desc-header">
+                    { item.header }
+                  </span>
+                  <ul className="sober-eye-section-left-desc-list">
+                    { item.list.map((i) => (
+                      <li>{ i }</li>
+                    )) }
+                  </ul>
+                  { item.extraInfo && (
+                    <>
+                      <span className="sober-eye-section-left-desc-extra-bolded">{ item.extraInfo.bolded }</span>
+                      <span className="sober-eye-section-left-desc-extra-thin">{ item.extraInfo.subtitle }</span>
+                    </>
+                  ) }
+                </div>
+              </div>
+            </>
+          ) : (
+              <>
+                <div className="sober-eye-section-half">
+                  <div className="sober-eye-section-left-desc-container">
+                    <span className="sober-eye-section-left-desc-header">
+                      { item.header }
+                    </span>
+                    <ul className="sober-eye-section-left-desc-list">
+                      { item.list.map((i) => (
+                        <li>{ i }</li>
+                      )) }
+                    </ul>
+                    { item.extraInfo && (
+                      <>
+                        <span className="sober-eye-section-left-desc-extra-bolded">{ item.extraInfo.bolded }</span>
+                        <span className="sober-eye-section-left-desc-extra-thin">{ item.extraInfo.subtitle }</span>
+                      </>
+                    ) }
+                  </div>
+                </div>
+                <div className="sober-eye-section-half">
+                  <img src={ item.image } alt="" />
+                </div>
+              </>
+            ) }
+
         </div>
       )) }
     </>
