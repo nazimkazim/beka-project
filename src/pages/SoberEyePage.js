5b44973b9843261f60e35c6c61c4199359ce077d
nazimkazim <nazik_111@mail.ru>
2020-08-22 13:45:56 +0600

    add sober eye

--- src/pages/SoberEyePage.js
+++ src/pages/SoberEyePage.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import SoberEyeVirtualGlass from '../images/sober-eye-virtual-glass.webp';
 
 export default function SoberEyePage() {
+ 
   return (
-    <div>
-      Sober eye page
+    <div className="sober-eye-section">
+      <div className="sober-eye-section-half">
+        <div className="sober-eye-section-left-desc-container">
+          <span className="sober-eye-section-left-desc-header">
+            Возможность пройти самостоятельный тест за минуту
+          </span>
+          <ul className="sober-eye-section-left-desc-list">
+            <li>Портативный и легкий в использовании</li>
+            <li>Не инвазивный</li>
+            <li>Результаты в реальном времени</li>
+          </ul>
+        </div>
+      </div>
+      <div className="sober-eye-section-half">
+      <img src={SoberEyeVirtualGlass} alt=""/>
+      </div>
     </div>
   )
 }
