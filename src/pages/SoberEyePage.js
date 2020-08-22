d485c10a75f5b109781ca4fedf51df8499228255
nazimkazim <nazik_111@mail.ru>
2020-08-22 13:58:29 +0600

    add object

--- src/pages/SoberEyePage.js
+++ src/pages/SoberEyePage.js
@@ -1,25 +1,52 @@
-import React from 'react'
+import React from 'react';
 import SoberEyeVirtualGlass from '../images/sober-eye-virtual-glass.webp';
+import Dashboardlaptop from '../images/dashboardlaptop.webp';
 
 export default function SoberEyePage() {
- 
+
+  const data = {
+    soberSectionsArr: [
+      {
+        header: 'Возможность пройти самостоятельный тест за минуту',
+        list: [
+          'Портативный и легкий в использовании', 'Не инвазивный', 'Результаты в реальном времени'
+        ],
+        image: SoberEyeVirtualGlass,
+        color:'#bfc6df'
+      },
+      {
+        header: 'Корпоративные отчеты в облаке',
+        list: [
+          'Мониторинг в реальном времени', 'Уведомления и предупреждения', 'Аналитика данных'
+        ],
+        image: Dashboardlaptop
+      }
+
+    ]
+  };
+
   return (
-    <div className="sober-eye-section">
-      <div className="sober-eye-section-half">
-        <div className="sober-eye-section-left-desc-container">
-          <span className="sober-eye-section-left-desc-header">
-            Возможность пройти самостоятельный тест за минуту
-          </span>
-          <ul className="sober-eye-section-left-desc-list">
-            <li>Портативный и легкий в использовании</li>
-            <li>Не инвазивный</li>
-            <li>Результаты в реальном времени</li>
-          </ul>
+    <>
+      { data.soberSectionsArr.map((item) => (
+        <div className="sober-eye-section" style={{backgroundColor:item.color}}>
+          <div className="sober-eye-section-half">
+            <div className="sober-eye-section-left-desc-container">
+              <span className="sober-eye-section-left-desc-header">
+                {item.header}
+            </span>
+              <ul className="sober-eye-section-left-desc-list">
+                {item.list.map((i) => (
+                  <li>{i}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div className="sober-eye-section-half">
+            <img src={ item.image } alt="" />
+          </div>
         </div>
-      </div>
-      <div className="sober-eye-section-half">
-      <img src={SoberEyeVirtualGlass} alt=""/>
-      </div>
-    </div>
-  )
+      )) }
+    </>
+
+  );
 }
