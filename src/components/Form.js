09b12ca399e6976b76e581db863c11918ccff0da
nazimkazim <nazik_111@mail.ru>
2020-09-05 20:01:21 +0600

    add fix form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,40 +1,22 @@
 import React from 'react';
 
 export default function Form() {
-  
+
   return (
-    <form method="POST" data-netlify="true">
-      <div class="field">
-        <p class="control has-icons-left">
-          <input class="input" type="text" name="name" placeholder="Имя" />
-          <span class="icon is-small is-left">
-            <i class="fa fa-user" aria-hidden="true"></i>
-          </span>
-        </p>
-      </div>
-      <div className="field">
-        <p className="control has-icons-left">
-          <input className="input" type="number" name="phone" placeholder="Телефон" />
-          <span className="icon is-small is-left">
-            <i className="fa fa-phone" aria-hidden="true"></i>
-          </span>
-        </p>
-      </div>
-      <div className="field">
-        <p className="control has-icons-left has-icons-right">
-          <input className="input" type="email" name="email" placeholder="Email" />
-          <span className="icon is-small is-left">
-            <i className="fas fa-envelope"></i>
-          </span>
-          <span className="icon is-small is-right">
-            <i className="fas fa-check"></i>
-          </span>
-        </p>
-      </div>
-      <div className="field">
-        <div data-netlify-recaptcha="true"></div>
-      </div>
-      <input className="button is-success" value="Отправить" type="submit"/>
+    <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+      <input type="hidden" name="form-name" value="contact" />
+      <p>
+        <label>Your Name: <input type="text" name="name" /></label>
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
     </form>
   );
 }
