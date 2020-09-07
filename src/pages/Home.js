6fa1609bc5e01858dfff1a268fdb167273674595
nazimkazim <nazik_111@mail.ru>
2020-09-07 18:36:19 +0600

    add change url

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,10 +12,6 @@ import { Helmet } from "react-helmet";
 const Home = () => {
   return (
     <>
-      <Helmet>
-        <meta charSet="utf-8" name="description" content="dfgfdgfdgdfg" />
-        <title>ertertert</title>
-      </Helmet>
       <Hero />
       <AboutUs />
       <WhyUs />
