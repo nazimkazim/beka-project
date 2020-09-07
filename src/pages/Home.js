815b319a03488d016c4c05c4b1d9d16b48959d12
nazimkazim <nazik_111@mail.ru>
2020-09-07 18:42:57 +0600

    add remove meta from index page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,6 @@ import React from "react";
 import Hero from "../components/Hero";
 import AboutUs from "../components/AboutUs";
 import WhyUs from "../components/WhyUs";
-import { Helmet } from "react-helmet";
 
 //import Banner from "../components/Banner";
 //import { Link } from "react-router-dom";
